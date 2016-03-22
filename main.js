3576a3ed31223e5003fed86b761e96577811aff4
twikle <twikle@users.noreply.github.com>
2016-03-21 20:26:16 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var dataset = [[5, 20], [480, 90], [250, 50], [100, 33], [330, 95], [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]];
-debugger
+
 var mappedData = dataset.map(function (item) {
   return {
     x: item[0],
